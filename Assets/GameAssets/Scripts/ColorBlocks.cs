678a58e4f0634f8142fb04d613ac7d8558e2c4f2
Osman Orhan Askin <osmanorhanaskin@gmail.com>
2026-03-24 11:01:42 +0300

    Update Camera Dir

--- Assets/GameAssets/Scripts/ColorBlocks.cs
+++ Assets/GameAssets/Scripts/ColorBlocks.cs
@@ -1,28 +1,28 @@
- using UnityEngine;              
+using UnityEngine;              
                                   
-  public class ColorBlock :       
-  MonoBehaviour   
-  {
-      public Color BlockColor {   
-  get; private set; }
-      public Vector2Int           
-  GridPosition { get; private set;
-   }
-      public bool IsCollected {
-  get; private set; }
+public class ColorBlock :       
+    MonoBehaviour   
+{
+    public Color BlockColor {   
+        get; private set; }
+    public Vector2Int           
+        GridPosition { get; private set;
+    }
+    public bool IsCollected {
+        get; private set; }
 
-      public void Init(Color
-  color, Vector2Int gridPos)
-      {
-          BlockColor = color;
-          GridPosition = gridPos;
-          GetComponent<Renderer>()
-  .material.color = color;
-      }
+    public void Init(Color
+        color, Vector2Int gridPos)
+    {
+        BlockColor = color;
+        GridPosition = gridPos;
+        GetComponent<Renderer>()
+            .material.color = color;
+    }
 
-      public void Collect()
-      {
-          IsCollected = true;
-          Destroy(gameObject);
-      }
-  }
+    public void Collect()
+    {
+        IsCollected = true;
+        Destroy(gameObject);
+    }
+}
